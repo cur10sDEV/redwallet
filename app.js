c78792f0bb948d1a1b74ce9cb778dac887c1d892
cur10sDEV <specsx@protonmail.com>
2022-11-21 16:45:44 +0530

    added cors

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
 
 //middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
